24d0c9c44015c976e7022480d1aed86155e5b627
Karpovich Yulia <375339008080.yuluya@gmail.com>
2025-10-22 17:57:42 +0300

    Time error

---
+++ 2.3.poiskTree.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <climits>
+using namespace std;
+int main()
+{
+    ifstream fin("bst.in");
+    ofstream fout("bst.out");
+    int n;
+    fin >> n;
+    if (n == 1) {
+        fout << "YES";
+        return 0;
+    }
+    int k;
+    fin >> k;
+    vector<long long> m(n);
+    m[0] = k;
+    vector<int> p(n);
+    vector<char> c(n);
+    
+    /*for (int i = 0; i < n - 1; i++) {
+        fin >> m[i + 1];
+        fin >> p[i];
+        fin >> c[i];
+    }*/
+    vector<long long> up(n, 0);
+    vector<long long> down(n, 0);
+
+    up[0] = (long long)INT_MAX+1;
+    down[0] = INT_MIN;
+
+    int i = 1;
+    bool ind = true;
+    while (i < n && ind) {
+
+        fin >> m[i];
+        fin >> p[i];
+        fin >> c[i];
+
+        int parent = p[i]-1;
+        if (c[i] == 'L') {
+            down[i] = down[parent];
+            up[i] = m[parent];
+        }
+        else if (c[i] == 'R') {
+            up[i] = up[parent];
+            down[i] = m[parent];
+        }
+        if (m[i] >= up[i] || m[i] < down[i])
+            ind = false;
+      
+      
+
+        i++;
+    }
+
+    if (ind) {
+        cout << "YES";
+        fout << "YES";
+    }
+    else {
+        cout << "NO";
+        fout << "NO";
+    }
+    return 0;
+}
